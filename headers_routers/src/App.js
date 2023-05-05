afd4014ae76dd906fe6eb795f2d7b5fdde47fa99
Nakshita <nakshita.malhotra.61@gmail.com>
2023-05-05 09:32:36 +0530

    started

---
+++ headers_routers/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Settings from "./Components/Settings";
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <div className = "App">
+        <h1>APP</h1>
+      </div>
+    );
+  }
+}
+ 
+export default App;
+
---
+++ headers_routers/src/Components/Settings.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="settings">
+                <h1>Settings</h1>
+            </div>
+        );
+    }
+}
+ 
+export default Settings;
